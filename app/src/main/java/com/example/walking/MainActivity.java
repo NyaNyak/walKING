85a0ec8d373cb17283aef0c55488f111d405ede3
NyaNyak <chsgood8182@naver.com>
2022-10-06 21:13:36 +0900

    ui 디자인

--- app/src/main/java/com/example/walking/MainActivity.java
+++ app/src/main/java/com/example/walking/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.walking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView gift;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gift = (ImageView) findViewById(R.id.gift);
+        gift.bringToFront();
     }
 }
